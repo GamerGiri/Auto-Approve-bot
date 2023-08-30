6889ce0219a4eb3c5d984745209d4de6edb5ddd4
MD Jackwan <92715067+GamerGiri@users.noreply.github.com>
2023-08-31 02:13:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "6ab604ff91a73fb91cc6526818e28ab1")
     BOT_TOKEN = getenv("BOT_TOKEN", "6210086334:AAEKhSp8l8PzmjKSbYQeCyUvm4RxH37S--I")
     FSUB = getenv("FSUB", "Krsbots")
-    CHID = int(getenv("CHID", "-1001829705047"))
+    CHID = int(getenv("CHID", "-1001779064622"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kartik:Kartik8379@cluster0.zrpl3tc.mongodb.net/?retryWrites=true&w=majority")
     
